791945abe39b5e51c17ce2b973235a26fc5f3a2e
Muhammet Oktay <muhammetokty@gmail.com>
2023-12-19 10:24:56 +0300

    Roboto font for body text

--- app/src/main/java/com/example/fitalife/ui/theme/Type.kt
+++ app/src/main/java/com/example/fitalife/ui/theme/Type.kt
@@ -3,6 +3,8 @@ package com.example.fitalife.ui.theme
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.Font
+
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
@@ -15,6 +17,11 @@ val Typography = Typography(
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     )
+
+    val RobotoRegular = FontFamily (
+        Font(R.font.)
+    )
+
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
