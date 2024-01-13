225218c36465ac3b8d661302c6a4e010e0e394d1
Muhammet Oktay <muhammetokty@gmail.com>
2024-01-13 11:32:07 +0300

    l

---
+++ app/build/tmp/kapt3/stubs/debug/com/example/fitalife/main/DietScreenKt.java
@@ -0,0 +1,25 @@
+package com.example.fitalife.main;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 8, 0}, k = 2, d1 = {"\u00002\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u000e\n\u0002\b\u0002\n\u0002\u0010\b\n\u0000\n\u0002\u0010\u000b\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\u001a>\u0010\u0000\u001a\u00020\u00012\u0006\u0010\u0002\u001a\u00020\u00032\u0006\u0010\u0004\u001a\u00020\u00052\u0006\u0010\u0006\u001a\u00020\u00052\u0006\u0010\u0007\u001a\u00020\b2\u0006\u0010\t\u001a\u00020\n2\f\u0010\u000b\u001a\b\u0012\u0004\u0012\u00020\u00010\fH\u0007\u001a\u0018\u0010\r\u001a\u00020\u00012\u0006\u0010\u0002\u001a\u00020\u00032\u0006\u0010\u000e\u001a\u00020\u000fH\u0007\u001a\u0010\u0010\u0010\u001a\u00020\u00012\u0006\u0010\u0002\u001a\u00020\u0003H\u0003\u00a8\u0006\u0011"}, d2 = {"DietCard", "", "navController", "Landroidx/navigation/NavController;", "dietId", "", "dietTitle", "dietImg", "", "isSelected", "", "onDietSelected", "Lkotlin/Function0;", "DietScreen", "vm", "Lcom/example/fitalife/AppViewModel;", "Header", "app_debug"})
+public final class DietScreenKt {
+    
+    @androidx.compose.runtime.Composable
+    public static final void DietScreen(@org.jetbrains.annotations.NotNull
+    androidx.navigation.NavController navController, @org.jetbrains.annotations.NotNull
+    com.example.fitalife.AppViewModel vm) {
+    }
+    
+    @androidx.compose.runtime.Composable
+    private static final void Header(androidx.navigation.NavController navController) {
+    }
+    
+    @androidx.compose.runtime.Composable
+    public static final void DietCard(@org.jetbrains.annotations.NotNull
+    androidx.navigation.NavController navController, @org.jetbrains.annotations.NotNull
+    java.lang.String dietId, @org.jetbrains.annotations.NotNull
+    java.lang.String dietTitle, int dietImg, boolean isSelected, @org.jetbrains.annotations.NotNull
+    kotlin.jvm.functions.Function0<kotlin.Unit> onDietSelected) {
+    }
+}
