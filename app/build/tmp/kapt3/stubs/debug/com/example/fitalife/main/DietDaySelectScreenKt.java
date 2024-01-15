b465d691029acc0ca05cd4f1e72e090484e7fbd9
AtakanD <111417517+ataduy@users.noreply.github.com>
2024-01-15 20:03:04 +0300

    Diyet ile ilgili tum ekranlar tamamen guncellendi, diger ekranlarda tasarimcilarin talebi uzerine minor degisiklikler yapildi.

---
+++ app/build/tmp/kapt3/stubs/debug/com/example/fitalife/main/DietDaySelectScreenKt.java
@@ -0,0 +1,20 @@
+package com.example.fitalife.main;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 8, 0}, k = 2, d1 = {"\u0000\u0016\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u000e\n\u0002\b\u0003\u001a \u0010\u0000\u001a\u00020\u00012\u0006\u0010\u0002\u001a\u00020\u00032\u0006\u0010\u0004\u001a\u00020\u00052\u0006\u0010\u0006\u001a\u00020\u0005H\u0007\u001a\u0018\u0010\u0007\u001a\u00020\u00012\u0006\u0010\u0002\u001a\u00020\u00032\u0006\u0010\u0006\u001a\u00020\u0005H\u0007\u00a8\u0006\b"}, d2 = {"DayCard", "", "navController", "Landroidx/navigation/NavController;", "dayName", "", "dietId", "DietDaySelectScreen", "app_debug"})
+public final class DietDaySelectScreenKt {
+    
+    @androidx.compose.runtime.Composable
+    public static final void DietDaySelectScreen(@org.jetbrains.annotations.NotNull
+    androidx.navigation.NavController navController, @org.jetbrains.annotations.NotNull
+    java.lang.String dietId) {
+    }
+    
+    @androidx.compose.runtime.Composable
+    public static final void DayCard(@org.jetbrains.annotations.NotNull
+    androidx.navigation.NavController navController, @org.jetbrains.annotations.NotNull
+    java.lang.String dayName, @org.jetbrains.annotations.NotNull
+    java.lang.String dietId) {
+    }
+}
---
+++ app/build/tmp/kapt3/stubs/debug/com/example/fitalife/main/DietPlanScreenKt.java
@@ -0,0 +1,14 @@
+package com.example.fitalife.main;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 8, 0}, k = 2, d1 = {"\u0000\u0016\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u000e\n\u0002\b\u0002\u001a \u0010\u0000\u001a\u00020\u00012\u0006\u0010\u0002\u001a\u00020\u00032\u0006\u0010\u0004\u001a\u00020\u00052\u0006\u0010\u0006\u001a\u00020\u0005H\u0007\u00a8\u0006\u0007"}, d2 = {"DietPlanScreen", "", "navController", "Landroidx/navigation/NavController;", "dietId", "", "day", "app_debug"})
+public final class DietPlanScreenKt {
+    
+    @androidx.compose.runtime.Composable
+    public static final void DietPlanScreen(@org.jetbrains.annotations.NotNull
+    androidx.navigation.NavController navController, @org.jetbrains.annotations.NotNull
+    java.lang.String dietId, @org.jetbrains.annotations.NotNull
+    java.lang.String day) {
+    }
+}
